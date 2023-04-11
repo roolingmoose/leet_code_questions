f6f5fa42490a81dcea59cb92995596a69c872b6c
roolingmoose <88731344+roolingmoose@users.noreply.github.com>
2023-04-11 11:42:16 +0530

    Create countcompletebinarytree.cpp

---
+++ countcompletebinarytree.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+Following is the Binary Tree node class
+    
+template <typename T = int>
+class TreeNode
+{
+public:
+    T data;
+    TreeNode<T> *left;
+    TreeNode<T> *right;
+
+    TreeNode(T val)
+    {
+        this->data = val;
+        left = NULL;
+        right = NULL;
+    }
+
+    ~TreeNode()
+    {
+        if (left != NULL)
+        {
+            delete left;
+        }
+        if (right != NULL)
+        {
+            delete right;
+        }
+    }
+};
+
+************************************************************/
+
+int countNodes(TreeNode<int> *root)
+{
+    int count=0 ;
+    if(root==NULL)  return 0;
+    count = countNodes(root->left)+ countNodes(root->right);
+
+   
+    return 1+count;
+
+
+
+
+
+}
